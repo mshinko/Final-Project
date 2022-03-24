7b226db7453117139f73776b98cd0738911a7600
Upsil0n <58853317+Upsil0n@users.noreply.github.com>
2022-03-24 08:00:31 -0400

    dictionary.txt updating

--- src/main/java/com/finalproject/application/Game.java
+++ src/main/java/com/finalproject/application/Game.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXML;
 public class Game {
     //file for game mechanics. I am unsure if specific labels and rectangles can be pulled from the controller class due to being unable to set the variables to static
 
-    //dictionary implementation; word check; update squares
+    //read dictionary.txt, import, use 5 letter words, then remove the ones from a different file that have been done previously
     private int currentRow = 0;
 
     private String word;
